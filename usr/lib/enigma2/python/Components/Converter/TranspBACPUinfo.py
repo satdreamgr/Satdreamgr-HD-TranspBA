a040dc4ce86172ad1bc387b4de76311b303a4609
Athanasios Oikonomou <athoik@gmail.com>
2019-01-03 20:38:20 +0200

    TranspBACPUinfo: change spaces with tabs

--- usr/lib/enigma2/python/Components/Converter/TranspBACPUinfo.py
+++ usr/lib/enigma2/python/Components/Converter/TranspBACPUinfo.py
@@ -6,30 +6,30 @@ import re
 
 class TranspBACPUinfo(Converter, object):
 
-        def __init__(self, type):
-                Converter.__init__(self, type)
-                self.type = type
+	def __init__(self, type):
+		Converter.__init__(self, type)
+		self.type = type
 
-        @cached
-        def getText(self):
-                if self.type == "CPUtemp":
-                        try:
-                                return self.getCPUtemp()
-                        except:
-                                pass
-                return ""
+	@cached
+	def getText(self):
+		if self.type == "CPUtemp":
+			try:
+				return self.getCPUtemp()
+			except:
+				pass
+		return ""
 
-        def getCPUtemp(self):
-                temp = ""
-                if os.path.exists('/proc/stb/fp/temp_sensor_avs'):
-                        temp = open('/proc/stb/fp/temp_sensor_avs').read().strip()
-                if os.path.exists('/sys/devices/virtual/thermal/thermal_zone0/temp'):
-                        temp = open('/sys/devices/virtual/thermal/thermal_zone0/temp').read()[:2]
-                if os.path.isfile("/proc/hisi/msp/pm_cpu"):
-                        temp = re.search('temperature = (\d+) degree', open("/proc/hisi/msp/pm_cpu").read()).group(1)
-                if temp:
-                        return _("%s°C") % temp
-                return ""
+	def getCPUtemp(self):
+		temp = ""
+		if os.path.exists('/proc/stb/fp/temp_sensor_avs'):
+			temp = open('/proc/stb/fp/temp_sensor_avs').read().strip()
+		if os.path.exists('/sys/devices/virtual/thermal/thermal_zone0/temp'):
+			temp = open('/sys/devices/virtual/thermal/thermal_zone0/temp').read()[:2]
+		if os.path.isfile("/proc/hisi/msp/pm_cpu"):
+			temp = re.search('temperature = (\d+) degree', open("/proc/hisi/msp/pm_cpu").read()).group(1)
+		if temp:
+			return _("%s°C") % temp
+		return ""
 
-        text = property(getText)
+	text = property(getText)
 
